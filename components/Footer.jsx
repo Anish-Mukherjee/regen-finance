0f5ae61e01d636d1ad534a9d11eb04f32dd6014f
Anish <cybertronian217@gmail.com>
2022-07-17 10:45:20 +0530

    Basic Page

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='h-10 bg-blue-600'>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-green-500 shadow-lg'>
+      <h1 className='text-3xl font-serif'>REGEN</h1>
+      <h1 className='text-3xl font-serif'>FINANCE</h1>
+    </div>
+  )
+}
+
+export default Navbar
